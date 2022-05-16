fb339176716dec8bb31248a7208601bd696c0aa1
Hasnain <hasnain_zahoor@yahoo.co.uk>
2022-05-16 12:33:32 +0100

    Helloworld

---
+++ src/main/java/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+        //String myString = "HelloWorld";
+        String myString = "My name is Hasnain";
+        System.out.println(myString);
+    }
+}
